284fcab55d25c56637f0ed9b5a637984a3ada2db
Gabriele-jpg <narutovolpe50@gmail.com>
2025-02-09 20:57:57 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,40 +2,42 @@
 
 #define DIM 1000
 
-int Simmetria(int matrix[DIM][DIM], int r) {
+int matrix[DIM][DIM]; // Dichiarata globalmente per evitare stack overflow
+
+int Simmetria(int matrix[][DIM], int r) {
     for (int i = 0; i < r; i++) {
-        for (int j = 0; j < i; j++) {
+        for (int j = 0; j < i; j++) { // Controlliamo solo sotto la diagonale principale
             if (matrix[i][j] != matrix[j][i]) {
-                return 0; // Non è simmetrica
+                return 0; // La matrice non è simmetrica
             }
         }
     }
-    return 1; // è simmetrica
+    return 1; // La matrice è simmetrica
 }
 
 int main() {
     int r;
-    int matrix[DIM][DIM];
 
-    printf("Inserisci la dimensione della matrice max %d: ", DIM);
-    scanf("%d", &r);
-
-    if (r > DIM || r <= 0) {
-        printf("errore: dimensione sbagliata.\n");
+    printf("Inserisci la dimensione della matrice (max %d): ", DIM);
+    if (scanf("%d", &r) != 1 || r > DIM || r <= 0) {
+        printf("Errore: dimensione non valida.\n");
         return 1;
     }
 
     printf("Inserisci gli elementi della matrice %d x %d:\n", r, r);
     for (int i = 0; i < r; i++) {
         for (int j = 0; j < r; j++) {
-            scanf("%d", &matrix[i][j]);
+            if (scanf("%d", &matrix[i][j]) != 1) {
+                printf("Errore: input non valido.\n");
+                return 1;
+            }
         }
     }
 
     if (Simmetria(matrix, r)) {
         printf("La matrice e' simmetrica.\n");
     } else {
-        printf("La matrice non e' speculare simmetrica.\n");
+        printf("La matrice non e' simmetrica.\n");
     }
 
     return 0;
